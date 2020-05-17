b01969819a4d02dcda4e3941a9db7ad9a5674ab3
amoswk <36827671+amoswk@users.noreply.github.com>
2020-05-17 14:26:31 -0400

    initial commit

---
+++ Gridy3/EditorViewController.swift
@@ -0,0 +1,135 @@
+//
+//  EditorViewController.swift
+//  GRIDYNEW
+//
+//  Created by Amos Katz on 4/19/20.
+//  Copyright © 2020 Amos Katz. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class EditorViewController: UIViewController {
+    
+    
+    //Variables for EditorViewController
+    var selectedImage: UIImage?
+    var initialCenter = CGPoint()
+    @IBOutlet weak var blurView: UIVisualEffectView!
+    @IBOutlet weak var imageWindow: UIView!
+    @IBOutlet weak var imageDisplay: UIImageView!
+    
+    
+    
+    
+    // FUNCTION for creating the window through the blur
+    
+    func setMask(with hole: CGRect, in view: UIView){
+        
+        
+        let mutablePath = CGMutablePath()
+        mutablePath.addRect(view.bounds)
+        mutablePath.addRect(hole)
+        
+        
+        let mask = CAShapeLayer()
+        mask.path = mutablePath
+        mask.fillRule = CAShapeLayerFillRule.evenOdd
+        
+        
+        view.layer.mask = mask
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+     imageDisplay.image = selectedImage
+
+    }
+    
+    
+    
+//    override func viewWillAppear(_ animated: Bool) {
+//         super.viewWillAppear(animated)
+//
+////                imageDisplay.image = selectedImage
+////
+////                so the blur view window loads on app startup
+//
+//               let squareWindow = imageWindow.frame.integral
+//
+//               // for setting the mask in the created view
+//               setMask(with: squareWindow, in: blurView!)
+        
+//
+//    }
+    
+    override func viewDidLayoutSubviews() {
+        
+        // so the blur view window loads on app startup
+
+//        let squareWindow = imageWindow.frame.integral
+        
+        let squareWindow = blurView.convert(imageWindow.frame, to: blurView)
+
+               // for setting the mask in the created view
+               setMask(with: squareWindow, in: blurView!)
+
+    }
+
+    
+    
+    
+    
+    //GESTURE Functions - Pinch, Pan, and Rotate
+    
+    
+    @IBAction func imagePinch(_ gestureRecognizer: UIPinchGestureRecognizer) {
+        guard imageDisplay != nil else { return }
+        
+        if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
+            imageDisplay.transform = (imageDisplay.transform.scaledBy(x: gestureRecognizer.scale, y: gestureRecognizer.scale))
+            gestureRecognizer.scale = 1.0
+        }
+        
+    }
+    
+    
+    
+    
+    @IBAction func imagePan(_ gestureRecognizer : UIPanGestureRecognizer) {
+        guard imageDisplay != nil else {return}
+        let piece = imageDisplay
+        
+        let translation = gestureRecognizer.translation(in: piece?.superview)
+        if gestureRecognizer.state == .began {
+            
+            self.initialCenter = piece!.center
+        }
+        
+        if gestureRecognizer.state != .cancelled {
+            
+            let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+            piece?.center = newCenter
+        }
+        else {
+            
+            piece?.center = initialCenter
+        }
+    }
+    
+    
+    
+    
+    @IBAction func imageRotate(_ gestureRecognizer : UIRotationGestureRecognizer) {
+        guard imageDisplay != nil else { return }
+        
+        if gestureRecognizer.state == .began || gestureRecognizer.state == .changed {
+            imageDisplay.transform = imageDisplay.transform.rotated(by: gestureRecognizer.rotation)
+            gestureRecognizer.rotation = 0
+        }
+    }
+    
+}
---
+++ Gridy3/ViewController.swift
@@ -0,0 +1,142 @@
+//
+//  ViewController.swift
+//  GRIDYNEW
+//
+//  Created by Amos Katz on 3/15/20.
+//  Copyright © 2020 Amos Katz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    let imagePicker = UIImagePickerController()
+    
+    let imgArray = [UIImage(named: "img1"), UIImage(named: "img2"), UIImage(named: "img3"), UIImage(named: "img4"), UIImage(named: "img5"), UIImage(named: "img6")].shuffled()
+    var selectedImage = UIImage()
+    
+    
+    
+    
+    
+    @IBOutlet weak var challengeLabel: UILabel!
+    
+    @IBOutlet weak var gridyLabel: UILabel!
+    
+    @IBOutlet weak var cameraButton: UIButton!
+    
+    @IBOutlet weak var libraryButton: UIButton!
+    
+    @IBOutlet weak var gridyButton: UIButton!
+    
+    
+    
+    //
+    
+    
+    override func viewDidLoad() {
+        
+        
+        
+        
+        
+        
+        super.viewDidLoad()
+        
+        
+        cameraButton.layer.cornerRadius = 15
+        libraryButton.layer.cornerRadius = 15
+        gridyButton.layer.cornerRadius = 15
+        
+        
+        
+        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        
+    }
+    
+    
+    
+    @IBAction func cameraButtonTouched(_ sender: Any) {
+        
+             if UIImagePickerController.isSourceTypeAvailable(.camera) {
+        
+        let actionOne = UIAlertController(title: "Photo Source", message: "Take a Photo", preferredStyle: .actionSheet)
+        actionOne.addAction(UIAlertAction(title: "Use Camera", style: .default, handler: { (action:UIAlertAction) in self.imagePicker.sourceType = .camera
+            
+        
+                
+                self.present(self.imagePicker, animated: true, completion: nil)
+                
+           
+            
+            
+        }))
+        
+        
+        self.present(actionOne, animated: true, completion: nil)
+         }
+    }
+    
+    @IBAction func libraryButtonTouched(_ sender: Any) {
+        
+        
+        
+        let actionTwo = UIAlertController(title: "Photo Source", message: "Choose a Photo", preferredStyle: .actionSheet)
+        actionTwo.addAction(UIAlertAction(title: "Open Library", style: .default, handler: { (action:UIAlertAction) in self.imagePicker.sourceType = .photoLibrary
+            self.present(self.imagePicker, animated: true, completion: nil)
+            
+        }))
+        
+        
+        self.present(actionTwo, animated: true, completion: nil)
+        
+        
+    }
+    
+    
+    // imagePickerController funciton allows camera or library image to move to next view
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print("Selected Image")
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let selectedImage = info[.originalImage] as? UIImage else {
+            
+            return }
+        
+        self.selectedImage = selectedImage
+        performSegue(withIdentifier: "showEditorView", sender: nil)
+        
+    }
+    
+    
+    
+    
+    
+    @IBAction func gridyPickButtonTouched(_ sender: Any) {
+        
+        
+        guard let firstImage = imgArray.shuffled().first, let image = firstImage else {
+            return
+        }
+        
+        self.selectedImage = image
+        
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        //using an identifier to identify the segue, named "showEditorView"
+        
+        if segue.identifier == "showEditorView" {
+            guard let editorVC = segue.destination as? EditorViewController else {
+                return
+            }
+            editorVC.selectedImage = self.selectedImage
+        }
+        
+        
+        
+    }
+    
+}
