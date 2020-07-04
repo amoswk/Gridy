9eaa39691521fb53072e0e846c241a0fbfad5695
amoswk <36827671+amoswk@users.noreply.github.com>
2020-07-04 01:49:40 -0400

    adding bezier path to main project

--- Gridy3/EditorViewController.swift
+++ Gridy3/EditorViewController.swift
@@ -57,53 +57,47 @@ class EditorViewController: UIViewController {
               
                 
 
-                
-                
-                let renderer1 = UIGraphicsImageRenderer(size: CGSize(width: 500, height: 500))
-                let img1 = renderer1.image { ctx in
                     
-                    ctx.cgContext.setStrokeColor(UIColor.red.cgColor)
-                    ctx.cgContext.setLineWidth(3)
-              
-                    
-                    ctx.cgContext.move(to: CGPoint(x: 500/3, y: 500/3))
-                    ctx.cgContext.addLine(to: CGPoint(x: 0, y: 500))
-                  
-                   
-                    
-            
-                    
-                    let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
-                    ctx.cgContext.addRect(rectangle)
-                    ctx.cgContext.drawPath(using: .fillStroke)
-                    
-                    
-                    
-                    
-                  
-                }
-                
-               let gridView = UIImageView(frame: squareWindow)
-                
-                
-                
-                gridView.backgroundColor = .clear
-                
-                gridView.image = img1
-                
-                self.imageWindow.addSubview(gridView)
-                
-            }
-    
+
     
             
         }
+    override func draw(_ rect: CGRect)
+    {
+        
+        var bezierPath = UIBezierPath()
+        bezierPath.lineWidth = 5.0
+        for index in 1...3 {
+            
+            let start = CGPoint(x: CGFloat(index) * self.bounds.width/4, y: 0)
+            let end = CGPoint(x: CGFloat(index) * self.bounds.width/4, y: self.bounds.height)
+            
+            let start2 = CGPoint(x: 0, y: CGFloat(index) * self.bounds.height/4)
+            let end2 = CGPoint(x: self.bounds.width, y: CGFloat(index) * self.bounds.height/4)
+            
+            
+            bezierPath.move(to: start)
+            bezierPath.addLine(to: end)
+            bezierPath.move(to: start2)
+            bezierPath.addLine(to: end2)
+            
+        }
+        
+        bezierPath.close()
+        
+        
+        UIColor.red.setStroke()
+        
+        
+        bezierPath.stroke()
+        
+        
+    }
     
     
     
     
-    
-    override func viewDidLoad() {
+  func viewDidLoad() {
         
         
         super.viewDidLoad()
@@ -118,7 +112,7 @@ class EditorViewController: UIViewController {
     
     
     
-        override func viewWillAppear(_ animated: Bool) {
+    func viewWillAppear(_ animated: Bool) {
              super.viewWillAppear(animated)
     
 
@@ -183,5 +177,7 @@ class EditorViewController: UIViewController {
         }
     }
     
-}
 
+
+}
+}
